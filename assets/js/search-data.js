b3587e06298b89d5136c80833032c04f66dfc2e4
modernTalker <116522860+modernTalker@users.noreply.github.com>
2025-10-08 23:02:40 +0000

    Deploying to gh-pages from @ modernTalker/modernTalker.github.io@4c077cdb3e909ae906e3751cef254be831a9501c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -418,14 +418,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6F%64%65%72%6E%74%61%6C%6B%65%72@%79%61%6E%64%65%78-%74%65%61%6D.%72%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/modernTalker", "_blank");
         },
       },{
         id: 'social-rss',
@@ -439,14 +439,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=AlQ_EUEAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
