6840843da387c113de8f5dd0da043265ea635156
modernTalker <116522860+modernTalker@users.noreply.github.com>
2025-10-12 16:07:04 +0000

    Deploying to gh-pages from @ modernTalker/modernTalker.github.io@2a4aa52ef5f84cdcf65a962d9000956d4085d65f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,13 +414,6 @@ ninja.data = [{
           window.open("https://github.com/modernTalker", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
